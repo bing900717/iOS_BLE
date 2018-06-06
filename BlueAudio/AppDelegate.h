ffb5d51dd2755aabc655855d533d7e7a22adf042
yaoxiaobing <bing900717@gmail.com>
2018-06-06 12:28:11 +0800

    Initial Commit

---
+++ BlueAudio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlueAudio
+//
+//  Created by 姚晓丙 on 2018/6/6.
+//  Copyright © 2018年 姚晓丙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlueAudio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlueAudio
+//
+//  Created by 姚晓丙 on 2018/6/6.
+//  Copyright © 2018年 姚晓丙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
